8ac7df3263888a42e5629e1affe787d53941213a
dobrishinov <dobrishinov@gmail.com>
2018-05-27 15:04:12 +0300

    Adding service in Startup, Implementing logic to return fake list with todo's in TodoController, deleting some non using views

--- Simple-ToDo/Controllers/TodoController.cs
+++ Simple-ToDo/Controllers/TodoController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Simple_ToDo.Models.TodoViewModels;
+using Simple_ToDo.Services;
 
 namespace Simple_ToDo.Controllers
 {
     public class TodoController : Controller
     {
-        public IActionResult Index()
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
         {
-            return View();
+            this._todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
+
+            return View(model);
         }
     }
 }
--- Simple-ToDo/Startup.cs
+++ Simple-ToDo/Startup.cs
@@ -37,6 +37,9 @@ namespace Simple_ToDo
             services.AddTransient<IEmailSender, EmailSender>();
 
             services.AddMvc();
+
+            services.AddSingleton<ITodoItemService, FakeTodoItemService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
