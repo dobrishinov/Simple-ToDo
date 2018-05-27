ced4d18bb58737c02109ac8eb77891712c6b1249
dobrishinov <dobrishinov@gmail.com>
2018-05-27 14:31:58 +0300

    Adding FakeTodoItem Service to simulate DB with hardcoded values, adding also Interface ITodoItemService

---
+++ Simple-ToDo/Services/ITodoItemService.cs
@@ -0,0 +1,13 @@
+﻿using Simple_ToDo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simple_ToDo.Services
+{
+    public interface ITodoItemService
+    {
+        Task<TodoItem[]> GetIncompleteItemsAsync();
+    }
+}
