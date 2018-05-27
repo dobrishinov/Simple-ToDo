d6c794450b29ca2325c9924ca0dac0a13ba5fa65
dobrishinov <dobrishinov@gmail.com>
2018-05-27 13:34:06 +0300

    Adding TodoController and TodoViewModels

---
+++ Simple-ToDo/Models/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simple_ToDo.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+
+        public bool IsDone { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public DateTimeOffset? DueAt { get; set; }
+    }
+}
---
+++ Simple-ToDo/Models/TodoViewModels/TodoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simple_ToDo.Models.TodoViewModels
+{
+    public class TodoViewModel
+    {
+        public TodoItem[] Items { get; set; }
+    }
+}
